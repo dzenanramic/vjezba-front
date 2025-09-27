aaf68c01a2ef9a123490df82f7d3854fd09a4a9a
dzenan <dzenan.ramic@hach.com>
2025-09-27 11:54:15 +0200

    ui improvement

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
-
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    allowedDevOrigins: [
-      'http://192.168.100.131',
-      'https://192.168.100.131'
-    ]
-  }
 };
 
 export default nextConfig;
